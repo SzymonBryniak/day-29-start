fa372d7ad2a2800275abd7783ac2ef1b72b6ba22
Szymon Bryniak <szymonbryniakproject@gmail.com>
2024-09-06 23:28:38 +0100

    password generator challenge progress

--- main.py.py
+++ main.py.py
@@ -1,8 +1,24 @@
 import tkinter
 from tkinter import *
 from tkinter import messagebox
-
+import random
 user_data = {}
+
+
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_comp = ([random.choice(letters) for char in range(nr_letters)] + [random.choice(numbers) for char in range(nr_symbols)] + [random.choice(symbols) for char in range(nr_numbers)])
+
+    print(password_comp)
 
 
 def store_data():
@@ -57,7 +73,7 @@ label2.grid(column=0, row=2)
 label3 = tkinter.Label(text='Password:')
 label3.grid(column=0, row=3)
 
-button1 = tkinter.Button(text='Generate Password')
+button1 = tkinter.Button(text='Generate Password', command="")
 button1.grid(column=1, row=3, columnspan=3, ipadx=11, sticky='e')
 
 
